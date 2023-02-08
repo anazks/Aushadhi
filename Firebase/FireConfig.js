3c83c34557f755d3912299317c48f1cc442f0812
anazks <anazksunil2@gmail.com>
2023-02-09 01:16:15 +0530

    first commit

---
+++ Firebase/FireConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI8regoUAYfFSKGqBJ29XFawwv4fXUAQo",
+    authDomain: "aushadhi-510e1.firebaseapp.com",
+    projectId: "aushadhi-510e1",
+    storageBucket: "aushadhi-510e1.appspot.com",
+    messagingSenderId: "592829299745",
+    appId: "1:592829299745:web:91bbed9f6c43d72afa3760",
+    measurementId: "G-4VR9391WY8"
+  };
